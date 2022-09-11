7f05e0daab19605bfbe30e84077d12dedcf14cdb
DonovanDumeige <donovan.dumeige@gmail.com>
2022-09-11 18:48:30 +0200

    Préparation JS

--- 03_TimeTracking_Dashboard/assets/script/script.js
+++ 03_TimeTracking_Dashboard/assets/script/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
+
 /* 
+Autre possibilité : agir sur les classes de mes boutons et celles
+du text des items. 
+Comme je l'ai entré main je n'ai qu'à choisir d'afficher daily, weekly ou monthly
+*/
+/* 
+
 * toute la logique de ce script repose sur fetch.
 todo Voir comment faire fonctionner fetch.
+Demande d'effacer l'entrée main et de garder les div, p vide. Il faudra alors totalement
+manipuler le DOM.
 */
