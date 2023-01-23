693d3ed6c21a3322c3098ca907fa3bd2bacf3bd8
Donovan Dumeige <donovan.dumeige@gmail.com>
2023-01-23 20:20:01 +0100

    feat: add logic

--- 03_TimeTracking_Dashboard/assets/script/script.js
+++ 03_TimeTracking_Dashboard/assets/script/script.js
@@ -1,51 +1,48 @@
 "use strict";
 
-
-/* 
-* toute la logique de ce script repose sur fetch.
-todo Voir comment faire fonctionner fetch.
-Demande d'effacer l'entrée main et de garder les div, p vide. Il faudra alors totalement
-manipuler le DOM.
-
-*/
 const 
-items = document.querySelectorAll('.item-container'),
 daily = document.querySelector('.slctDaily'),
 weekly = document.querySelector('.slctWeekly'),
 montlhy = document.querySelector('.slctMonthly'),
 now = document.querySelectorAll('.now'),
-previous = document.querySelectorAll('.now');
+previous = document.querySelectorAll('.previous');
+let data;
 
-fetch("assets/data/data.json").then(response =>{
-    if(response.ok)
-    {
-        response.json()
-        .then(traitement)
-        .catch(error => console.log(error))
+async function handleFetch() {
+    data = await fetch("./assets/data/data.json");
+    if(data.ok === true){
+        data = await data.json()
+        daily.addEventListener("click", getData);
+        weekly.addEventListener("click", getData);
+        montlhy.addEventListener("click", getData);
     }
-});
+}
+handleFetch();
 
-function traitement(data)
-{
-    console.log(data.length)
 
-    // La boucle for permet d'avancer dans les données JSON
-    for(let i=0; i<data.length; i++){
-        console.log(data[i].title)
-        console.log(`${data[i].title}
-        now = ${data[i].timeframes.daily.current}hrs
-        previous = ${data[i].timeframes.daily.previous} hrs`
-        )
-
-        /* 
-        Permet d'afficher les éléments récupérés du JSON et les stocker dans l'HTML.
-        todo affichage daily/weekly/monthly à gérer
-        */
-        items[i].innerHTML =
-        `<h3 class="item-title">${data[i].title}</h3>
-        <p class="now">${data[i].timeframes.daily.current} hr(s)</p>
-        <p class="previous"> Last day : ${data[i].timeframes.daily.previous} hr(s)</p>`
-        
-        
-    } // end loop for
+function getData(e) {
+    const time = e.target.textContent.toLowerCase() 
+    switch (time) {
+        case "daily":
+            for(let i = 0; i < now.length; i++) {
+                now[i].innerHTML = `Current : ${data[i].timeframes.daily.current}h`;
+                previous[i].innerHTML =`Last day - ${data[i].timeframes.daily.previous}h`
+            }
+            break;
+        case "weekly":
+            for(let i = 0; i < now.length; i++) {
+                now[i].innerHTML = `Current : ${data[i].timeframes.weekly.current}h`;
+                previous[i].innerHTML =`Last week - ${data[i].timeframes.weekly.previous}h`
+            }
+                break;
+        case "monthly":
+            for(let i = 0; i < now.length; i++) {
+            now[i].innerHTML = ` Current : ${data[i].timeframes.monthly.current}h`;
+            previous[i].innerHTML =`Last month - ${data[i].timeframes.monthly.previous}h`
+            }
+            break;
+                
+        default:
+            break;
+    }
 }
