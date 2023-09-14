2ddee6a1fa70336936a3a84dc21213d8f747779e
Doneuts <ddumeige.dev@gmail.com>
2023-09-14 22:21:22 +0200

    feat : finish logic for the challenge. Add a foreach loop to get the value and event listener to show it on the dedicated area

--- 04_RatingForm/assets/script/script.js
+++ 04_RatingForm/assets/script/script.js
@@ -2,25 +2,43 @@
 
 /**
  * *------ Steps -----
- * 1. Récupérer la valeur au clic sur l'un des boutons
- * 2. Faire apparaitre le contenetextContent)ur thanks__wrapper et disparaitre le contenur rating__wrapper
- * 3. Afficher la valeur récupérée dans le span rate__area
+ * 1. Get a value from a rate by a click.
+ * 2. Apply style from .isActive for the chosen rate.
+ * 3. Make disapear .rating__wrapper and show thanks__wrapper
+ * 4. Show the rate on the area .rate__area
  */
 
-let rates = document.querySelectorAll(".feedback__content");
-console.log(rates);
-let rate;
-rates.forEach((r) => r.addEventListener("click", getValue));
+// ? First method : forEach
 
-function getValue(b) {
-  console.log(b);
-  console.log(b.target);
-  /*  let result = e.currentTarget.textContent.trim();
-  return result;
+/* 
+! I forgot something crucial with forEach. This loop can't return a value and use a callback function as a parameter.
+! If I try to return something, the result will be undefined.
+! All the execution has to be inside the forEach to work well.
  */
-  /*   console.log(this.textContent.trim());
-  return this.textContent.trim(); */
-}
+const rates = document.querySelectorAll(".feedback__content");
+const rating_wrapper = document.querySelector(".rating__wrapper");
+const thanks_wrapper = document.querySelector(".thanks__wrapper");
+const btn = document.querySelector(".btn");
+const rate_area = document.querySelector("#rate__area");
 
-rate = getValue();
-console.log("Test", rate);
+let rate = null;
+rates.forEach((r) =>
+  r.addEventListener("click", (e) => {
+    // checked and unchecked behaviour
+    const active = document.querySelector(".isActive");
+    if (active) {
+      active.classList.remove("isActive");
+    }
+    const rateContainer = e.target;
+    rateContainer.classList.add("isActive");
+    rate = e.target.textContent;
+  })
+);
+
+btn.addEventListener("click", () => {
+  if (rate) {
+    rate_area.innerText = rate;
+    rating_wrapper.classList.add("isHidden");
+    thanks_wrapper.classList.remove("isHidden");
+  }
+});
